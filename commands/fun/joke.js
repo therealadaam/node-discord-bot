78b3e0f1575c3a74d6708f3f566db45c130788a9
Adam Rabenstein <therealadaam@gmail.com>
2021-06-22 02:22:08 -0700

    updated joke for better wording \ also created mute.js and tested, seems to be working after testing.

--- commands/fun/joke.js
+++ commands/fun/joke.js
@@ -3,7 +3,7 @@ module.exports = {
 	name: 'joke',
 	description: 'Might tell jokes one day',
 	execute(message) {
-		message.reply("Nah bruv, I don't joke around");
+		// message.reply("Nah bruv, I don't joke around");
 		const url = 'https://api.chucknorris.io/jokes/random';
 		fetch(url)
 			.then((res) => res.json())
---
+++ commands/moderation/mute.js
@@ -0,0 +1,43 @@
+module.exports = {
+	name: 'mute',
+	description: 'Tag a member to mute them for the specified amount of minutes.',
+	guildOnly: true,
+	permissions: 'KICK_MEMBERS',
+	usage: '[@userToMute <minutesToMute>]',
+	args: true,
+	execute(message, args) {
+		if (!message.mentions.users.size) {
+			return message.reply('you need to tag a user in order to mute them!');
+		}
+		console.log(`${args.length}`);
+		if (args.length < 2) {
+			return message.reply('you need to specify how long to mute them!');
+		}
+
+		const mutedRoleId = '856796084536737792';
+		const mutedRole = message.guild.roles.cache.get(mutedRoleId);
+		const timeInMinutes = args[1];
+		const timeInMS = timeInMinutes * 60 * 1000;
+		const taggedUser = message.mentions.members.first();
+
+		// get existing roles
+		// console.log(taggedUser);
+		const oldRoles = taggedUser.roles.cache.clone();
+		// console.log(mutedRole);
+
+		taggedUser.roles.set([mutedRole]);
+
+		message.channel.send(
+			`${taggedUser.user.username} is in timeout and has been muted for ${timeInMinutes}`
+		);
+		taggedUser.send(
+			`You're in timeout! You can talk in #shit-posting and that's it.`
+		);
+
+		setTimeout(() => {
+			// console.dir(oldRoles);
+			// console.dir([oldRoles]);
+			taggedUser.roles.set(oldRoles);
+		}, timeInMS);
+	},
+};
